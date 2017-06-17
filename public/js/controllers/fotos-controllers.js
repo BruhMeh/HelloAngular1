f9669365ab4e0a9b4f7e706e119de44b94c32279
BsdCode <bruno.sdelgado@outlook.com>
2017-06-17 09:28:43 -0300

    Buscando lista de imagens via Promise

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -1,17 +1,16 @@
-angular.module('alurapic').controller('FotosController', function($scope){
-  $scope.fotos = [
-    {
-      titulo : "Leão",
-      url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    },
-    {
-      titulo : "Leão2",
-      url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    },
-    {
-      titulo : "Leão3",
-      url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    }
+angular.module('alurapic').controller('FotosController', function($scope , $http){
+   $scope.fotos = [];
 
-  ];
+   $http.get('v1/fotos')
+   .success(function(fotos){
+      $scope.fotos = fotos;
+   })
+   .error(function(erro){
+      console.log(erro);
+   })
+   promise.then(function(retorno){
+     $scope.fotos = retorno.data;
+  }).catch(function(error){
+    console.log(error);
+ });
 });
