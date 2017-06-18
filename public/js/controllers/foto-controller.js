6cc8377b874433a4db0d6ff082e641e49b68ba11
BsdCode <bruno.sdelgado@outlook.com>
2017-06-18 12:21:16 -0300

    Refactor da validação

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,14 +1,16 @@
 angular.module('alurapic').controller('FotoController', function($scope , $http){
    $scope.foto = {};
+   $scope.mensagem = "";
 
    $scope.submeter = function(){
       if($scope.formulario.$valid){
          $http.post('v1/fotos' , $scope.foto)
          .success(function(){
             $scope.foto = {};
-            console.log('Foto inserida com sucesso');
+            $scope.mensagem ='Foto inserida com sucesso';
          })
          .error(function(erro){
+            $scope.mensagem = 'Foto não foi inserida';
             console.log(erro);
          });
       }
