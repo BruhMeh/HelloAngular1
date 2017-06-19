5758ba80ecc9e0d0c8a943a849ae781d03dd5287
BsdCode <bruno.sdelgado@outlook.com>
2017-06-19 01:46:23 -0300

    Criado edit usando put

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,18 +1,40 @@
-angular.module('alurapic').controller('FotoController', function($scope , $http){
+angular.module('alurapic').controller('FotoController', function($scope , $http, $routeParams){
    $scope.foto = {};
    $scope.mensagem = "";
 
+   if($routeParams.fotoId){
+      $http.get('v1/fotos/' + $routeParams.fotoId)
+      .success(function (foto) {
+         $scope.foto = foto;
+      })
+      .error(function (erro) {
+         console.log(erro);
+         $scope.mensagem ='Não foi possivel obter a foto de ID' + $routeParams.fotoId;
+      });
+   }
+
    $scope.submeter = function(){
       if($scope.formulario.$valid){
-         $http.post('v1/fotos' , $scope.foto)
-         .success(function(){
-            $scope.foto = {};
-            $scope.mensagem ='Foto inserida com sucesso';
-         })
-         .error(function(erro){
-            $scope.mensagem = 'Foto não foi inserida';
-            console.log(erro);
-         });
+         if($scope.foto._id){
+            $http.put('v1/fotos/' + $routeParams.fotoId , $scope.foto)
+            .success(function () {
+               $scope.mensagem ='Foto '+ $scope.foto.titulo+' alterada com sucesso';
+            })
+            .error(function (erro) {
+               console.log(erro);
+               $scope.mensagem ='Não foi possivel alterar a foto ' + $scope.foto.titulo;
+            });
+         }else{
+            $http.post('v1/fotos' , $scope.foto)
+            .success(function(){
+               $scope.foto = {};
+               $scope.mensagem ='Foto inserida com sucesso';
+            })
+            .error(function(erro){
+               $scope.mensagem = 'Foto não foi inserida';
+               console.log(erro);
+            });
+         }
       }
    };
 });
