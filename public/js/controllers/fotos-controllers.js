03910756db409e0ef405be2626e53f28e9954ef3
BsdCode <bruno.sdelgado@outlook.com>
2017-06-19 01:23:54 -0300

    Adicionado remove no menu principal

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -1,6 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope , $http){
    $scope.fotos = [];
    $scope.filtro = '';
+   $scope.mensagem = '';
 
    $http.get('v1/fotos')
    .success(function(fotos){
@@ -15,4 +16,17 @@ angular.module('alurapic').controller('FotosController', function($scope , $http
  //  }).catch(function(error){
  //    console.log(error);
  // });
+
+   $scope.remover = function(foto) {
+      $http.delete('v1/fotos/'+foto._id)
+      .success(function () {
+         var indiceFoto = $scope.fotos.indexOf(foto);
+         $scope.fotos.splice(indiceFoto, 1);
+         $scope.mensagem ='Foto '+foto.titulo+' foi removida com sucesso';
+      })
+      .error(function (erro) {
+         console.log(erro);
+         $scope.mensagem = 'Foto '+foto.titulo+' não pode ser removida';
+      });
+   };
 });
